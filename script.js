687fe64d12b6c4175e6a07522a933b4e2d57b0ce
DaniAkiode <dannyakiode@hotmail.co.uk>
2021-11-09 12:55:05 +0000

    corulsel not working

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ $(document).ready(function() {
         }else{
             $('.navbar').removeClass('sticky');
         }
+
+            // scroll-up button show/hide script
+            if(this.scrollY > 500){
+                $('.scroll-up-btn').addClass("show");
+            }else{
+                $('.scroll-up-btn').removeClass("show");
+            }
     });
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
@@ -12,4 +19,28 @@ $(document).ready(function() {
         $('.menu-btn i').toggleClass('active')
         
     })
-})
+
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+
+    
+});
