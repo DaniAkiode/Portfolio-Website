35b176f14f82201d6b839af63faafc2a31432d57
DaniAkiode <dannyakiode@hotmail.co.uk>
2021-11-12 12:53:09 +0000

    Template complete

--- script.js
+++ script.js
@@ -21,13 +21,27 @@ $(document).ready(function() {
         $('html').css("scrollBehavior", "auto");
     });
 
-    
+
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass('active')
         $('.menu-btn i').toggleClass('active')
         
     })
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Post-Graduate", "Web Developer", "Software Developer", "Creator", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing-2", {
+        strings: ["Post-Graduate", "Web Developer", "Software Developer", "Creator", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
 
     $('.carousel').owlCarousel({
         margin: 20,
